778c309e1c97cb0618334c0957c4712c82e891f8
jarcane <jarcane@users.noreply.github.com>
2016-12-13 21:07:42 +0200

    Implement Eq and Ord for Rollers

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ extern crate rand;
 mod parse;
 mod random;
 
+use std::cmp::Ordering;
+
 use pest::*;
 use parse::*;
 
@@ -38,4 +40,24 @@ impl<'a> Roller<'a> {
      pub fn total(&self) -> i32 {
          self.total
      }
+}
+
+impl<'a> PartialEq for Roller<'a> {
+    fn eq(&self, other: &Roller) -> bool {
+        self.total == other.total
+    }
+}
+
+impl<'a> Eq for Roller<'a> {}
+
+impl<'a> Ord for Roller<'a> {
+    fn cmp(&self, other: &Roller) -> Ordering {
+        self.total.cmp(&other.total)
+    }
+}
+
+impl<'a> PartialOrd for Roller<'a> {
+    fn partial_cmp(&self, other: &Roller) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
