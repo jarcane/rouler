235483dd039239f84bc46d46aacf0c2bb6176f25
jarcane <jarcane@users.noreply.github.com>
2016-12-17 01:25:30 +0200

    Proof of concept: generic die generators

--- src/random.rs
+++ src/random.rs
@@ -7,8 +7,32 @@
 
 use rand::{thread_rng, Rng};
 
-pub fn roll_dice_raw(num: i64, sides: u64) -> i64 {
-    let mut rng = thread_rng();
+// pub fn roll_dice_raw(num: i64, sides: u64) -> i64 {
+//     let mut rng = thread_rng();
 
-    (0..num.abs()).map(|_| rng.gen_range(1, sides as i64 + 1)).fold(0, |acc, x| acc + x)
+//     (0..num.abs()).map(|_| rng.gen_range(1, sides as i64 + 1)).fold(0, |acc, x| acc + x)
+// }
+
+trait Die : Iterator<Item=i64> {}
+
+impl<T: Iterator<Item=i64>> Die for T {}
+
+pub struct StdDie {
+    sides: u64
+}
+
+impl Iterator for StdDie {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<i64> {
+        Some(thread_rng().gen_range(1, self.sides as i64 + 1))
+    }
+}
+
+fn roll_dice_gen<T: Die>(num: i64, die: T) -> i64 {
+    die.take(num as usize).fold(0, |acc, x| acc + x)
+}
+
+pub fn roll_dice_raw(num: i64, sides: u64) -> i64 {
+    roll_dice_gen(num, StdDie{sides: sides})
 }
